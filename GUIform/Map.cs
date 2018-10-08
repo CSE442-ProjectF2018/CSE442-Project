e8414b3b283fe5ee33e1d293c38b93b031c93df4
Dylan Degenhart <dylandegenhart@hotmail.com>
2018-10-08 09:35:43 -0400

    Branched off to work on Map class.

---
+++ GUIform/Map.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUIform
+{
+    class Map
+    {
+        //Default dimension is 16x16.
+        private int dimension = 16;
+        //Default map type(empty). Int: for use with switch statement.
+        private int mapType = 0;
+        
+        //If no arguments given, use default arguments.
+        public Map()
+        {
+            buildMap();
+        }
+        
+        //Parameter d refers to dimension(s) of the grid. It's a square so only one number is needed.
+        public Map(int d, int mt)
+        {
+            dimension = d;
+            mapType = mt;
+
+            buildMap();
+        }
+
+        public buildMap()
+        {
+            Block[,] info = new Block[dimension,dimension];
+
+            for(int i = 0; i < dimension; ++i)
+            {
+                for(int j = 0; j < dimension; ++j)
+                {
+                    info[i, j] = new Block();
+                }
+            }
+        }
+    }
+}
