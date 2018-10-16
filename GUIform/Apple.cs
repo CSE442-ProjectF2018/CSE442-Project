004535a26c9f0540989242fd428c79bd15f86bd3
Dylan Degenhart <dylandeg@buffalo.edu>
2018-10-16 19:44:07 -0400

    "Merged" Apple and Snake classes.

---
+++ GUIform/Apple.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUIform
+{
+
+
+    public class Apple
+    {
+        //Apple has x and y coordinates
+        public int x = 0;
+        public int y = 0;
+
+        public void placeA(int i, int j)
+        {
+            if (Master.Apples > 0)   //Apples suser has left
+            {
+                x = i;
+                y = j;
+                Master.Apples--;
+            }
+            return;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+
+        public System.Drawing.Color selectColor(System.Drawing.Color color)
+        {
+            return color;
+        }
+    }
+
+
+}
---
+++ GUIform/Snake.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUIform
+{
+    public enum direction { up, down, left, right }
+
+    public class SnakeNode
+    {
+        int x;
+        int y;
+        SnakeNode* next;
+        SnakeNode* prev;
+
+    }
+
+    public class Snake
+    {
+        SnakeNode* Head;
+        SnakeNode* Tail;
+        int speed;
+        direction dir;
+        int length = 2;
+
+        public void spawn(int x, int y, int len)
+        {
+            Head = new SnakeNode();
+            Head.x = x;
+            Head.y = y;
+            Tail = new SnakeNode();
+            Tail.x = x;
+            Tail.y = y;
+
+            SnakeNode* tempnode;
+            tempnode = Head;
+
+            for (int i = 2; i < len; i++)
+            {
+                tempnode.next = new SnakeNode();
+                tempnode.next.x = gridx;
+                tempnode.next.y = gridy;
+                tempnode.next.prev = tempnode;
+                tempnode = tempnode.next;
+                tempnode.next = Tail;
+                Tail.prev = tempnode;
+                length = length + 1;
+
+            }
+
+        }
+
+        public void slither(direction d)
+        {
+
+        }
+
+        public void consume(int gridx, int gridy)
+        {
+
+        }
+
+        public void perish()
+        {
+
+        }
+
+    }
+
+}
