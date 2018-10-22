381de8b3766ec841784758e1d1cb578b2b1c8875
bhowell3 <bhowell3@bufffalo.edu>
2018-10-22 00:52:47 -0400

    Snake can be spawned with at least two nodes, keeps track of size, all nodes have same grid position

--- GUIform/Snake.cs
+++ GUIform/Snake.cs
@@ -6,37 +6,58 @@ using System.Threading.Tasks;
 
 namespace GUIform
 {
+    //snake movement
     public enum direction { up, down, left, right }
 
     public class SnakeNode
     {
-        int x;
-        int y;
-        SnakeNode* next;
-        SnakeNode* prev;
+        //grid position for slice of snake
+        private int x;
+        private int y;
+
+        //next moves towards the tail
+        private SnakeNode next;
+        //prev moves towads the head
+        private SnakeNode prev;
 
     }
 
     public class Snake
     {
-        SnakeNode* Head;
-        SnakeNode* Tail;
-        int speed;
-        direction dir;
-        int length = 2;
+        //head is the part that looks like it moves
+        public SnakeNode Head;
+        //tail is what actually moves
+        public SnakeNode Tail;
 
-        public void spawn(int x, int y, int len)
+        //record of previous direction
+        public direction dir;
+
+        //keeps track of snake length
+        //just incase we want to use it for something
+        private int length = 2;
+
+        //unemplemented
+        public int speed;
+
+        //this builds the snake
+        public void spawn(int gridx, int gridy, int len=2)
         {
+            //entire snake begins in one grid space
+
+            //start with at least two nodes
+            //one for the head
             Head = new SnakeNode();
-            Head.x = x;
-            Head.y = y;
+            Head.x = gridx;
+            Head.y = gridy;
+            //one for the tail
             Tail = new SnakeNode();
-            Tail.x = x;
-            Tail.y = y;
+            Tail.x = gridx;
+            Tail.y = gridy;
 
-            SnakeNode* tempnode;
+            //if a longer snake is desired
+            //this loop adds them
+            SnakeNode tempnode;
             tempnode = Head;
-
             for (int i = 2; i < len; i++)
             {
                 tempnode.next = new SnakeNode();
