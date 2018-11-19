ee6afe9e47d2528cb5c2e137e5d3f95b13de280f
Lloyd <lloydmur@buffalo.edu>
2018-11-19 12:53:11 -0500

    Here are the assets

--- GUIform/Form1.cs
+++ GUIform/Form1.cs
@@ -39,6 +39,7 @@ namespace GUIform
         SoundPlayer s_AppleGET;
         SoundPlayer s_snakeDIE;
         SoundPlayer s_Player;
+        SoundPlayer s_hover;
 
 
         //gif image related
@@ -543,7 +544,7 @@ namespace GUIform
             Panel p = (Panel)sender;
             if (_yourTurn)
             {
-                play_SFX("panel.wav");
+                //play_SFX("panel.wav");
                 if (p.BackgroundImage == null) p.BackgroundImage = i_hover;
             }
         }
